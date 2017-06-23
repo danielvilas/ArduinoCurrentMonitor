b3d72a84bfcae86894ae9f9564f9bc919b2f6820
Daniel Vilas <daniel.vilas@gmail.com>
2017-06-23 20:14:50 +0200

    New App for Visualization

--- MatlabAnalisys/analisysSizeSamplesError.m
+++ MatlabAnalisys/analisysSizeSamplesError.m
@@ -1,14 +1,27 @@
-function [sizes, outSizes] = analisysSizeSamplesError( packet )
+function [sizes, outSizes] = analisysSizeSamplesError( packet, mode )
 %analisysSizeSamples Summary of this function goes here
 %   Detailed explanation goes here
 
+if ~ exist('mode','var')
+    modeVal=0;
+elseif strcmp(mode,'error')
+    modeVal=0;
+elseif strcmp(mode,'average')
+    modeVal=1;
+else
+    modeVal=0;
+end
     
     sizes= [];
     outSizes=[1:50];
     
     for sizeSamples=1:50
         [data, dataOut]=calcSize(packet,sizeSamples);
-        sizes=[sizes; max(abs(data - mean(data)))];
+        if modeVal==0
+            sizes=[sizes; max(abs(data - mean(data)))];
+        elseif modeVal==1
+            sizes=[sizes; mean(data)];
+        end
         outSizes(sizeSamples)=sizeSamples;
     end
 end
